28b65698c4efe71fabe436740c94fb2d93a8d7a2
João Gabriel Lopes <jglopes6@gmail.com>
2018-10-10 18:36:20 -0300

    Comentários para w_adjlist.cpp

--- classes/w_adjlist.cpp
+++ classes/w_adjlist.cpp
@@ -1,6 +1,9 @@
 #include "w_adjlist.h"
 
 w_AdjList::w_AdjList(string FileLocation){
+    // Construtor para a lista de Adjacências a partir de um arquivo externo
+    // contendo o grafo
+
     ifstream GraphFile;
     GraphFile.open(FileLocation);
 
@@ -25,7 +28,7 @@ w_AdjList::w_AdjList(string FileLocation){
 
 
 void w_AdjList::addEdge(int from, int to, int num_vertices, double weight){
-    // Insere um vértice na estrutura
+    // Insere um vértice novo na estrutura de dados
 
     if (to < num_vertices){
         AdjList_Tuple adjacent;
@@ -45,7 +48,7 @@ void w_AdjList::addEdge(int from, int to, int num_vertices, double weight){
 
 
 void w_AdjList::print(){
-    // Printa no console a representação da matriz
+    // Printa no console a representação da lista de adjacência
 
     for (int i = 0; i < adjlist.size(); i++){
         cout << "Adjacency adjlist of vertex " << i << endl;
@@ -61,27 +64,36 @@ void w_AdjList::print(){
 
 
 vector < double > w_AdjList::Dijkstra(int root){
+    // Implementação do algoritmo de Dijkstra para apenas um vértice
+    // Retorna o vetor de distâncias no final do Dijkstra do vértice escolhido para todos os outros
+
     // Corrige o erro de off by one
     root = root-1;  
 
     // Cria distâncias do maior valor possível (infinito)
     vector < double > dist (num_vertices, __DBL_MAX__);
-
     dist[root] = 0;
 
+    // Cria o conjunto de vértices restantes para serem percorridos
+    // Set faz a ordenação automática a partir do valor das distâncias
     set< pair<double, int> > remaining_vertices;
     remaining_vertices.insert( {0, root} );
 
 
     while (!remaining_vertices.empty()) {        
         
+        // Escolhe os vértices de menor distância
         double current_dist = remaining_vertices.begin()->first;
         int current_vertex = remaining_vertices.begin()->second;
 
+        // Remove o vértice do set, indicando que foi percorrido
         remaining_vertices.erase( {current_dist, current_vertex});
 
+        // Itera sobre os vizinhos do vértice atual sendo percorrido
         for (list<AdjList_Tuple>::iterator it = adjlist[current_vertex].begin(); it != adjlist[current_vertex].end(); ++it){
             int neighbor = it->connected_vertex; 
+
+            // Caso tenha achado um caminho melhor, ajusta a distância e insere no set
             if (dist[neighbor] > dist[current_vertex] + it->weight) {
                 dist[neighbor] = dist[current_vertex] + it->weight;
                 remaining_vertices.insert( {dist[neighbor], neighbor});
@@ -89,29 +101,42 @@ vector < double > w_AdjList::Dijkstra(int root){
         }
     }
 
+    // Retorna o vetor das distâncias
     return dist;
 }
 
+
+
 vector < int > w_AdjList::findPathFromParent (vector < int > parent, int target){
+    // Recebe o vetor de pais e calcula o caminho do vértice original até o vértice alvo
+
     vector < int > path;
     int iter = parent[target];
     path.push_back(iter);
+
+    // Anda de pai a pai até achar um vértice sem pai (a raíz) escrevendo num vetor
     while (parent[iter] != -1) {
         iter = parent[iter];
         path.push_back(iter);
     }
 
+    // Printa o vetor de trás pra frente seguindo assim o caminho raiz->alvo 
     for (int i = path.size()-1; i > -1; i--){
         //Ajusta o off by one
         cout << path[i]+1 << " ";
     }
     cout << target+1 << " " << endl;
+
+    // Retorna vértice do caminho
     return path;
 }
 
 pair < double, vector <int> > w_AdjList::Dijkstra_target(int root, int target){
-    // Calcula a menor distância entre dois vértices
-    // Corrige erros off by one
+    // Implementação do algoritmo de Dijkstra para apenas um vértice
+    // Double do pair representa a distância entre a raíz e o alvo
+    // Retorna o caminho entre o vetor raíz e o alvo
+
+    // Corrige erros de off by one
     root--;
     target--;
 
@@ -119,17 +144,23 @@ pair < double, vector <int> > w_AdjList::Dijkstra_target(int root, int target){
     vector < double > dist (num_vertices, __DBL_MAX__);
     dist[root] = 0;
 
+    // Cria vetor com os pais dos vetores para fazer o caminho
     vector < int > parent (num_vertices, -1);
-
+    
+    // Cria o conjunto de vértices restantes para serem percorridos
+    // Set faz a ordenação automática a partir do valor das distâncias
     set< pair<double, int> > remaining_vertices;
     remaining_vertices.insert( {0, root} );
 
 
     while (!remaining_vertices.empty()) {        
         
+        // Escolhe os vértices de menor distância
         double current_dist = remaining_vertices.begin()->first;
         int current_vertex = remaining_vertices.begin()->second;
 
+        // Checa se o vértice escolhido é o alvo
+        // Se sim, retorna
         if (current_vertex == target) {
             pair < double, vector <int> > ans;
             ans.first = dist[current_vertex];
@@ -137,10 +168,14 @@ pair < double, vector <int> > w_AdjList::Dijkstra_target(int root, int target){
             return ans;
         }
 
+        // Remove o vértice do set, indicando que foi percorrido
         remaining_vertices.erase( {current_dist, current_vertex});
-
+        
+        // Itera sobre os vizinhos do vértice atual sendo percorridos
         for (list<AdjList_Tuple>::iterator it = adjlist[current_vertex].begin(); it != adjlist[current_vertex].end(); ++it){
             int neighbor = it->connected_vertex; 
+
+            // Caso tenha achado um caminho melhor, ajusta a distância, insere no set e estabelece o pai
             if (dist[neighbor] > dist[current_vertex] + it->weight) {
                 dist[neighbor] = dist[current_vertex] + it->weight;
                 remaining_vertices.insert( {dist[neighbor], neighbor});
@@ -148,6 +183,8 @@ pair < double, vector <int> > w_AdjList::Dijkstra_target(int root, int target){
             }
         }
     }
+
+    // Caso não tenha sido encontrado caminho, retorna -1 e o último caminho percorrido
     pair < double, vector <int> > ans;
     ans.first = -1;
     ans.second = findPathFromParent(parent, target);
@@ -157,29 +194,43 @@ pair < double, vector <int> > w_AdjList::Dijkstra_target(int root, int target){
 
 
 double w_AdjList::Prim(int root, bool output = false){
+    // Calcula a MST do grafo partindo do vértice root, retornando seu custo total
+    // Se output for true, gera um arquivo MST.txt com a MST codificada
+
     // Corrige o erro de off by one
     root = root-1;  
 
+    // Cria distâncias do maior valor possível (infinito)
     vector < double > cost (num_vertices, __DBL_MAX__);
     cost[root] = 0;
 
+    // Cria vetor com os pais dos vetores para fazer o caminho
     vector < int > parent (num_vertices, -1);
 
+    // Cria vetor indicando se o vértice escolhido já está na MST ou não
     vector < bool > inMST (num_vertices, false);
-
+ 
+    // Cria o conjunto de vértices restantes para serem percorridos
+    // Set faz a ordenação automática a partir do valor dos custos
     set< pair<double, int> > remaining_vertices;
     remaining_vertices.insert( {0, root} );
     
+
     while (!remaining_vertices.empty()){
+        
+        // Escolhe os vértices de menor distância
         double current_cost = remaining_vertices.begin()->first;
         int current_vertex = remaining_vertices.begin()->second;
 
+        // Remove o vértice do set e marca, indicando que foi percorrido
         remaining_vertices.erase( {current_cost, current_vertex});
-        
         inMST[current_vertex] = true;
 
+        // Itera sobre os vizinhos do vértice atual sendo percorridos
         for (list<AdjList_Tuple>::iterator it = adjlist[current_vertex].begin(); it != adjlist[current_vertex].end(); ++it){
             int neighbor = it->connected_vertex; 
+
+            // Caso tenha achado um custo melhor para um vértice fora da MST, ajusta a distância, insere no set e estabelece o pai
             if (cost[neighbor] > it->weight && inMST[neighbor] == false) {
                 cost[neighbor] = it->weight;
                 remaining_vertices.insert( {cost[neighbor], neighbor});
@@ -188,10 +239,12 @@ double w_AdjList::Prim(int root, bool output = false){
         }
     }
 
+    // Se há pedido para gerar a MST em texto, chama a função de graph.cpp que trata
     if (output){
         outputMinimumSpanningTree(&parent[0], &cost[0]);
     }
 
+    // Itera sobre o vetor de custo para achar o custo total
     double total_cost = 0;
     for (int i = 0; i < num_vertices; i++){
         total_cost += cost[i];
@@ -202,7 +255,9 @@ double w_AdjList::Prim(int root, bool output = false){
 
 
 pair < vector < double >, vector <int> > w_AdjList::Dijkstra_core(int root){
-    // CHAMADO DE DENTRO DAS FUNÇÕES, NÃO TEM OFF BY ONE
+    // Dijkstra feito para ser chamado de dentro das funções
+    // Retorna o vetor de distâncias e o vetor de pais
+    // Como é chamado por uma função, não tem off by one
 
 
     // Cria distâncias do maior valor possível (infinito)
@@ -212,19 +267,26 @@ pair < vector < double >, vector <int> > w_AdjList::Dijkstra_core(int root){
     // Cria vetor para armazenar os pais dos vértices
     vector < int > parent (num_vertices, -1);
 
+    // Cria o conjunto de vértices restantes para serem percorridos
+    // Set faz a ordenação automática a partir do valor das distâncias
     set< pair<double, int> > remaining_vertices;
     remaining_vertices.insert( {0, root} );
 
 
     while (!remaining_vertices.empty()) {        
         
+        // Escolhe os vértices de menor distância
         double current_dist = remaining_vertices.begin()->first;
         int current_vertex = remaining_vertices.begin()->second;
 
+        // Remove o vértice do set, indicando que foi percorrido
         remaining_vertices.erase( {current_dist, current_vertex});
 
+        // Itera sobre os vizinhos do vértice atual sendo percorrido
         for (list<AdjList_Tuple>::iterator it = adjlist[current_vertex].begin(); it != adjlist[current_vertex].end(); ++it){
             int neighbor = it->connected_vertex; 
+
+            // Caso tenha achado um caminho melhor, ajusta a distância e insere no set
             if (dist[neighbor] > dist[current_vertex] + it->weight) {
                 dist[neighbor] = dist[current_vertex] + it->weight;
                 remaining_vertices.insert( {dist[neighbor], neighbor});
@@ -232,6 +294,8 @@ pair < vector < double >, vector <int> > w_AdjList::Dijkstra_core(int root){
             }
         }
     }
+
+    // Retorna o vetor das distâncias e dos pais num pair
     pair < vector < double >, vector <int> > ans;
     ans.first = dist;
     ans.second = parent;
@@ -241,14 +305,18 @@ pair < vector < double >, vector <int> > w_AdjList::Dijkstra_core(int root){
 
 
 double w_AdjList::excentricity(int root){
+    // Retorna a excentricidade do grafo no vértice
+
     // Corrige erros off by one
     root--;
 
     double max = 0;
     int max_vertex = -1;
     
+    // Procura as distâncias para todos os vértices chamando Dijkstra
     vector <double> dist = Dijkstra_core(root).first;
     
+    // Itera o vetor de distâncias procurando a distância máxima
     for (int vertex = 0; vertex < num_vertices; vertex++){
         if (dist[vertex] > max) {
             max = dist[vertex];
@@ -260,12 +328,20 @@ double w_AdjList::excentricity(int root){
 }
 
 
+
 double w_AdjList::medium_distance(){
+    // Retorna a distância média do grafo fazendo Dijkstra em todos os pontos
+    // Desconsidera para cálculo da soma de distâncias quando a distância for infinta
+
     double sum_of_distances = 0;
     double amount_of_distances = (num_vertices*(num_vertices-1))/2;
     
+    // Itera calculando Dijkstra sobre todos os vértices do grafo
     for (int root = 0; root < num_vertices; root++){
         vector <double> dist = Dijkstra_core(root).first;
+
+        // Itera sobre a distância de Dijkstra somando as distâncias
+        // Começar o enlace em root evita cálculos redundantes
         for (int vertex = root; vertex < num_vertices; vertex++){
             if (dist[vertex] != __DBL_MAX__) {
                 sum_of_distances += dist[vertex];
@@ -273,13 +349,17 @@ double w_AdjList::medium_distance(){
         }
     }
 
+    // Divide as distâncias pela quantidade de medidas
     double ans = (sum_of_distances / amount_of_distances);
 
     return ans;
 }
 
 double w_AdjList::medium_distance_probabilistic(int iterations){
-    
+    // Retorna uma estimativa para a distância média calculando multiplas vezes a partir de vértices aleatórios
+    // Iterations estabelece quantas medidas vão ser tomadas
+
+    // Estabelecendo a geração aleatória de vértices
     mt19937 rng;
     rng.seed(random_device()());
     uniform_int_distribution<mt19937::result_type> rand_vertex(1, getNumVertices()-1);
@@ -287,8 +367,10 @@ double w_AdjList::medium_distance_probabilistic(int iterations){
     double sum_of_distances = 0;
     double amount_of_distances = 0;
 
+    // Pela quantidade de iterações oferecidas
     for (int i = 0; i < iterations; i++){
 
+        // Calcula as distâncias a partir dos valores no Dijkstra
         vector <double> dist = Dijkstra_core(rand_vertex(rng)).first;
         for (int vertex = 0; vertex < num_vertices; vertex++){
             if (dist[vertex] != __DBL_MAX__) {
@@ -298,6 +380,7 @@ double w_AdjList::medium_distance_probabilistic(int iterations){
         }
     }
     
+    // Divide as distâncias pela quantidade de medidas
     double ans = (sum_of_distances / amount_of_distances);
     return ans;
 }
@@ -307,15 +390,22 @@ int w_AdjList::vertexDegree(int vertex) {
     // Retorna o grau de um dado vértice
 
     int total = 0;
+    
+    // Itera sobre os vizinhos
     for (list<AdjList_Tuple>::iterator it = adjlist[vertex].begin(); it != adjlist[vertex].end(); ++it){
         total += 1;
     }
     return total; 
 }
 
-vector<int> w_AdjList::three_Max_Degrees() { 
+vector<int> w_AdjList::three_Max_Degrees() {
+    // Encontra os 3 maiores vértices
+    // Solução rápida e não expansível feita apenas para os estudos de caso
+    
+    // Vetor para armazenar os graus
     vector < int > degrees (num_vertices, -1);
 
+    // Procura o maior grau
     int max1 = 0; 
     int i_max1 = -1; 
     for (int i = 0; i < num_vertices; i++) {
@@ -326,6 +416,7 @@ vector<int> w_AdjList::three_Max_Degrees() {
         }
     }
 
+    // Procura o maior grau dentre os que restam
     int max2 = 0;
     int i_max2 = -1;
     for (int i = 0; i < num_vertices; i++) {
@@ -335,7 +426,7 @@ vector<int> w_AdjList::three_Max_Degrees() {
         }
     }
 
-
+    // Procura o maior grau dentre os que restam
     int max3 = 0;
     int i_max3 = -1;
     for (int i = 0; i < num_vertices; i++) {
@@ -345,17 +436,19 @@ vector<int> w_AdjList::three_Max_Degrees() {
         }
     }
 
-    vector<int> answer {i_max1, i_max2, i_max3, max1, max2, max3};
+    // Ajuste do off by one nos indices
+    vector<int> answer {i_max1+1, i_max2+1, i_max3+1, max1, max2, max3};
 
-    for (int i = 0; i <6; i++){
-        cout << answer[i] << " ";
-    }
-    cout << endl;
     return answer;
 }
 
 void w_AdjList::print_neighbours(int vertex){
+    // Printa os vizinhos de um vértice
+
+    // Corrige o off by one
     vertex--;
+
+    // Itera sobre os vizinhos, printando e corrigindo o off by one
     for (list<AdjList_Tuple>::iterator it = adjlist[vertex].begin(); it != adjlist[vertex].end(); ++it){
         cout << it->connected_vertex+1 << " ";
     }
